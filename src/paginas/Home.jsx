2f6b05307d2ff01fb910d8907b9ede07e63feb45
BrandonAACode <brandon.arenas02@inacapmail.cl>
2025-07-09 00:13:04 -0400

    Agregue mensaje para el home

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,11 +1,14 @@
-import Portada from '../componentes/Portada';
 
 function Home() {
   return (
-    <>
-      <Portada />
-        </>
+    <div className="container mt-5 pt-5 text-azulrey">
+  <h1 className="mb-4"><i>Bienvenido</i></h1>
+  <p><b><i>
+    <strong>ConstruRedes</strong> donde fusionamos la solidez de la construcción con la conectividad del futuro. Nos especializamos en crear espacios seguros, modernos y totalmente conectados.Nuestro compromiso es potenciar tus ideas y construir un mundo a tu medida, con profesionalismo, innovación y cercanía en cada proyecto.
+  </i></b></p>
+</div>
   );
 }
+
 
 export default Home;
