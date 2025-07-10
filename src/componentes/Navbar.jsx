8a37b6f4068fab50bf9f3072d87e315ac699808c
BrandonAACode <brandon.arenas02@inacapmail.cl>
2025-07-10 00:13:23 -0400

    tamaños

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
       <div className="container">
         {/* Logo + nombre empresa */}
         <Link className={`navbar-brand fw-bold d-flex align-items-center ${scrolled ? 'text-white' : 'text-dark'}`} to="/">
-          <img src="/logo.jpg" alt="Logo" height="40" className="me-2" />
+          <img src={`${import.meta.env.BASE_URL}logo.jpg`} alt="Logo" height="50" />
           ConstruRedes
         </Link>
 
